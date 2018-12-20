b1d5ddeb9196bb4641cbe4ff0d6da5e8ad1d48f1
a.churilin <ch257@yandex.ru>
2018-12-20 11:36:02 +0500

    IniParser

--- scripts/modules/IniParser.py
+++ scripts/modules/IniParser.py
@@ -17,6 +17,18 @@ class IniParser:
 		line = line.replace(' ', '')
 		return line
 		
+	def parse_line(self, line):
+		section = None
+		param = None
+		value = None
+		if line[0:1] == '[' and line[-1:] == ']':
+			section = line
+		else:
+			eq_smb = line.find('=')
+			param = line[0:eq_smb]
+			value = line[eq_smb + 1:]
+		return section, param, value
+		
 	def read_ini(self, ini_file_path, encoding):
 		if self.errors.error_occured:
 			return self.settiings
@@ -27,8 +39,8 @@ class IniParser:
 			line = ini_file.read_line()
 			if line:
 				line = self.exlude_non_data(line)
-				
-				print(line)
+				section, param, value = self.parse_line(line)
+				print(section, param, value)
 			else:
 				break
 		ini_file.close_file()
