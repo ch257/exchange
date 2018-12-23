308acfb9aaa07aed8a252ac6f8f9d1e1a07c2b4c
ch257 <ch257@yandex.ru>
2018-12-23 22:15:19 +0500

    get param from ini

--- scripts/modules/ClassTemplate.py
+++ scripts/modules/ClassTemplate.py
@@ -7,7 +7,8 @@ class ClassTemplate:
 	def __init__(self):
 		self.errors = Errors()
 		self.ini_encoding = 'utf-8'
-		self.settings = []
+		self.ini_parser = IniParser(self.errors)
+		self.settings = {}
 	
 	def read_settings(self, args):
 		if len(args) < 2:
@@ -18,12 +19,15 @@ class ClassTemplate:
 				encoding = args[2]
 				
 			ini_file_path = args[1]
-			ini_parser = IniParser(self.errors)
-			self.settings = ini_parser.read_ini(args[1], encoding)
+			self.ini_parser.read_ini(args[1], encoding)
 	
 	def main(self, args):
 		self.read_settings(args)
-			
+		self.settings['g1'] = self.ini_parser.get_param('g1', None)
+		self.settings['g2'] = {}
+		self.settings['g2']['i4'] = self.ini_parser.get_param('g2', 'i4')
+		
+		print(self.settings)
 		if self.errors.error_occured:
 			self.errors.print_errors()
 		else:
--- scripts/modules/IniParser.py
+++ scripts/modules/IniParser.py
@@ -5,7 +5,7 @@ from modules.Files import *
 class IniParser:
 	def __init__(self, errors):
 		self.errors = errors
-		self.settiings = {}
+		self.settings = {}
 	
 	def exlude_non_data(self, line):
 		line = line.rstrip('\n')
@@ -22,7 +22,7 @@ class IniParser:
 		param = None
 		value = None
 		if line[0:1] == '[' and line[-1:] == ']':
-			section = line
+			section = line[1:-1]
 		else:
 			eq_smb = line.find('=')
 			param = line[0:eq_smb]
@@ -35,13 +35,40 @@ class IniParser:
 		
 		ini_file = Files(self.errors)
 		ini_file.open_file(ini_file_path, 'r', encoding)
+		last_section = None
 		while not self.errors.error_occured:
 			line = ini_file.read_line()
 			if line:
 				line = self.exlude_non_data(line)
 				section, param, value = self.parse_line(line)
-				print(section, param, value)
+				if section:
+					self.settings[section] = {}
+					last_section = section
+				if last_section  and param:
+					self.settings[last_section][param] = value
+				
 			else:
 				break
 		ini_file.close_file()
-		return self.settiings
+		self.settings['ini_file_path'] = ini_file_path
+		return self.settings
+		
+	def get_param(self, section, param, c_type = 'str'):
+		if self.errors.error_occured:
+			return None
+			
+		if self.settings.get(section):
+			if param:
+				if self.settings[section].get(param):
+					if c_type == 'int':
+						return int(self.settings[section][param])
+					elif c_type == 'float':
+						return float(self.settings[section][param])
+					else:
+						return self.settings[section][param]
+				else:
+					self.errors.raise_error('No parameter [' + section + '][' + param + '] in ini file ' + self.settings['ini_file_path'])
+			else:
+				return self.settings[section]
+		else:
+			self.errors.raise_error('No section [' + section + '] in ini file ' + self.settings['ini_file_path'])
