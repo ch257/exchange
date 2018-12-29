0d1c04a8a1cd1af618fb4c1987b438739ed59949
a.churilin <ch257@yandex.ru>
2018-12-29 11:06:27 +0500

    subplots

--- scripts/modules/common/Plotter.py
+++ scripts/modules/common/Plotter.py
@@ -14,7 +14,16 @@ class Plotter:
 	def __init__(self, errors):
 		self.errors = errors
 		self.settings = {}
+		self.ax = []
+		self.subplot_offset = 0
 		
+	def create_subplot(self, subplot_index, subplots_number, subplot_height_share):
+		if subplot_index == 0:
+			self.ax.append(plt.subplot2grid((sum(subplot_height_share) , 1), (self.subplot_offset, 0), rowspan = subplot_height_share[subplot_index]))
+		else:
+			self.ax.append(plt.subplot2grid((sum(subplot_height_share) , 1), (self.subplot_offset, 0), rowspan = subplot_height_share[subplot_index], sharex = self.ax[0]))
+		
+		self.subplot_offset += subplot_height_share[subplot_index]
 		
 	def plot_series(self, data, settings, fig_name):
 		if self.errors.error_occured:
@@ -22,14 +31,11 @@ class Plotter:
 		
 		fig_folder = settings['plotter']['fig_folder']
 		series = settings['plotter']['series']
-		subplot_height_share = settings['plotter']['subplot_height_share'] 
 		subplots_number = settings['plotter']['subplots_number']
+		subplot_height_share = settings['plotter']['subplot_height_share'] 
 		
-		ax = []
-		offset = 0
-		for i in range(subplots_number):
-			ax.append(plt.subplot2grid((sum(subplot_height_share) , 1),(offset , 0), rowspan = subplot_height_share[i]))
-			offset = offset + subplot_height_share[i]
+		for subplot_index in range(subplots_number):
+			self.create_subplot(subplot_index, subplots_number, subplot_height_share)
 			
 		plt.tight_layout()
 		plt.savefig(fig_folder + 'fig' + fig_name + '.png', dpi = 100)
